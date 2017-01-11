253a76ffc1d52f451b5e5e21ee744f42e8d94b0c
BorisZ-git <ribos@bk.ru>
2017-01-12 00:15:46 +0300

    add lines class create construct

---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        public VerticalLine (int UpY, int DownY, int x, char sym)
+        {
+
+        }
+    }
+}
