d5d741d4709dab6368942d16e09d119d02af5941
BorisZ-git <ribos@bk.ru>
2017-01-11 23:23:04 +0300

    test class list

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,7 +14,28 @@ namespace Snake
             p1.Draw();
             Point p2 = new Point( 5, 4, '#');
             p2.Draw();
+            Point p3 = new Point(10, 10, '@');
+            Point p4 = new Point(6, 9, '&');
 
+            List<char> symList = new List<char>();
+            symList.Add('#');
+            symList.Add('&');
+            symList.Add('@');
+            symList.Add('*');
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            
+            foreach (char i in symList)
+            {
+                Console.WriteLine(i);
+            }
+            foreach (Point p in pList)
+            {
+                Console.WriteLine(p.x);
+            }
 
             Console.ReadLine();
         }
